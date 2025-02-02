ddc4a46450cf87fed1f2b4f53c68a4be29b12048
22yashika1 <yashikarao22j@gmail.com>
2025-02-02 16:44:31 +0530

    modified

--- questions/q17.py
+++ questions/q17.py
@@ -1,3 +1,4 @@
 #Write a Python program to display the examination schedule.
 examination_start_date= (13,12,2025)
 print("The examination will commence from : %i/%i/%i" % examination_start_date)
+# '%i' placeholders are filled with the values from the 'exam_st_date' tuple
